dd26609d0db83be651754073f6f02d0464713b59
Abhinav Shetty <01abhinavshetty@gmail.com>
2026-09-07 11:22:27 +0530

    Update day02.sql

--- Day-02-GroupBy/day02.sql
+++ Day-02-GroupBy/day02.sql
@@ -6,7 +6,7 @@ SELECT AVG(CAST(Salary AS DECIMAL(10,2))) AS AverageSalary FROM Employees;
 \--3.
 SELECT MAX(Salary) AS HighestSalary FROM Employees;
 \--4.
-SELECT Department, COUNT(\*) AS EmployeeCount FROM Employees
+SELECT Department, COUNT(*) AS EmployeeCount FROM Employees
 GROUP BY Department;
 \--5.
 SELECT Department, AVG(CAST(Salary AS DECIMAL(10,2))) AS AverageSalary FROM Employees
@@ -15,15 +15,15 @@ GROUP BY Department;
 SELECT Department, MAX(Salary) AS MaxSalary FROM Employees
 GROUP BY Department;
 \--7.
-SELECT Department, COUNT(*) AS EmployeeCount FROM Employees*
-*GROUP BY Department HAVING COUNT(*) > 3;
+SELECT Department, COUNT(*) AS EmployeeCount FROM Employees
+GROUP BY Department HAVING COUNT(*) > 3;
 \--8.
 SELECT Department, AVG(CAST(Salary AS DECIMAL(10,2))) AS AverageSalary FROM Employees
 GROUP BY Department HAVING AVG(CAST(Salary AS DECIMAL(10,2))) > 70000;
 \--9.
-SELECT Department, COUNT(*) AS EmployeeCount FROM Employees*
-*WHERE Salary > 70000*
-*GROUP BY Department HAVING COUNT(*) >2;
+SELECT Department, COUNT(*) AS EmployeeCount FROM Employees
+WHERE Salary > 60000
+GROUP BY Department HAVING COUNT(*) >2;
 \--10.
 SELECT TOP 1
        Department,
